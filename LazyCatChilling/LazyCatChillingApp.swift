70ee2c694dd1c273f4561fab28901a7cfc2babbc
Caojin <596730256@qq.com>
2025-04-09 14:01:51 +0800

    Initial Commit

---
+++ LazyCatChilling/LazyCatChillingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyCatChillingApp.swift
+//  LazyCatChilling
+//
+//  Created by Allen on 2025/4/9.
+//
+
+import SwiftUI
+
+@main
+struct LazyCatChillingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LazyCatChillingTests/LazyCatChillingTests.swift
@@ -0,0 +1,17 @@
+//
+//  LazyCatChillingTests.swift
+//  LazyCatChillingTests
+//
+//  Created by Allen on 2025/4/9.
+//
+
+import Testing
+@testable import LazyCatChilling
+
+struct LazyCatChillingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
